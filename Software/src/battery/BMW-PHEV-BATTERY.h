6f96889fa70e8032bdc26a8af419bda4eef50881
rha <reijo@oh1gwk.org>
2024-11-23 07:55:48 +0200

    BMW phev module

---
+++ Software/src/battery/BMW-PHEV-BATTERY.h
@@ -0,0 +1,10 @@
+#ifndef BMW_PHEV_BATTERY_H
+#define BMW_PHEV_BATTERY_H
+#include "../include.h"
+
+#define BATTERY_SELECTED
+#define MAX_CELL_DEVIATION_MV 9999
+
+void setup_battery(void);
+
+#endif
